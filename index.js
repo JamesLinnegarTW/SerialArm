3b27f4d231b606f9540969550473508a9bf69377
James Linnegar <jlinnega@thoughtworks.com>
2019-02-07 16:49:37 +0000

    Changes to certs and auth

--- index.js
+++ index.js
@@ -2,77 +2,83 @@ var awsIot = require('aws-iot-device-sdk');
 var SerialPort = require('serialport');
 
 
-var port = new SerialPort(process.env.PORT, {
+var arm = new SerialPort(process.env.PORT, {
   baudRate: 9600
 });
 
-port
-  .on('open', function (err) {
-    console.log('Connected to Arm');
-    setTimeout(()=> {
-      port.write("c");
-    }, 1500);
-  })
-  .on('data', function(data) {
-    console.log("A:", data.toString())
-  })
-  .on('error', function(error){
-    console.log(error);
-  });
+
+function handleMessage(topic, payload){
+  const data = JSON.parse(payload.toString());
+  switch(data.instruction){
+    case "wake":
+      arm.write("w");
+      break;
+    case "sleep":
+      arm.write("s");
+      break;
+    case "raiseArm":
+      arm.write("u");
+      break;
+    case "phone":
+      arm.write("x");
+      break;
+    case "lowerArm":
+      arm.write("d");
+      break;
+
+    case "rotateArm":
+      arm.write(`a${data.data}`);
+      break;
+
+    case "rotateHand":
+      arm.write(`h${data.data}`);
+      break;
+
+    case "pickup":
+      arm.write("p");
+      break;
+
+    case "putDown":
+      arm.write("o");
+      break;
+
+    default:
+      console.log("unknown instruction", data);
+      break;
+}
 
 
-
-var broker = awsIot.device({
-   keyPath: './certs/MacbookPro.private.key',
-  certPath: './certs/MacbookPro.cert.pem',
-    caPath: './certs/root-CA.crt',
-  clientId: process.env.CLIENT,
-      host: process.env.HOST
+const device = awsIot.device({
+   keyPath: "./certs/MacBookSemiPro.private.key",
+  certPath: "./certs/MacBookSemiPro.cert.pem",
+    caPath: "./certs/root-CA.crt",
+  clientId: "robot-arm",
+      host: "a2hsm6d1kkjcod-ats.iot.us-east-1.amazonaws.com"
 });
 
-
-broker
+device
   .on('connect', function() {
-    console.log('Connected to Broker');
-    broker.subscribe('/arm');
-  })
-  .on('message', function(topic, payload) {
-    const data = JSON.parse(payload.toString());
-    switch(data.instruction){
-      case "wake":
-        port.write("w");
-        break;
-      case "sleep":
-        port.write("s");
-        break;
-      case "raiseArm":
-        port.write("u");
-        break;
-      case "phone":
-        port.write("x");
-        break;
-      case "lowerArm":
-        port.write("d");
-        break;
-
-      case "rotateArm":
-        port.write(`a${data.data}`);
-        break;
-
-      case "rotateHand":
-        port.write(`h${data.data}`);
-        break;
-
-      case "pickup":
-        port.write("p");
-        break;
-
-      case "putDown":
-        port.write("o");
-        break;
-
-      default:
-        console.log("unknown instruction", data);
-        break;
-    }
+    device.subscribe('arm');
   });
+
+device
+  .on('message', function(topic, payload) {
+    console.log('message', topic, payload.toString());
+  });
+
+device
+   .on('close', function() {
+      console.log('close');
+   });
+device
+   .on('reconnect', function() {
+      console.log('reconnect');
+   });
+device
+   .on('offline', function() {
+      console.log('offline');
+   });
+device
+   .on('error', function(error) {
+      console.log('error', error);
+   });
